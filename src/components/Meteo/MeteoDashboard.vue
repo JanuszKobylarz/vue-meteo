42019646f336af6b1b7e9638f21829a39ad2f13c
Janusz Kobylarz <j.kobylarz@live.com>
2024-11-12 19:20:20 +0100

    First chart

--- src/components/Meteo/MeteoDashboard.vue
+++ src/components/Meteo/MeteoDashboard.vue
@@ -1,49 +1,49 @@
 <script lang="ts" setup>
-    import { fetchWeatherApi } from 'openmeteo';
+import ky from 'ky';
+import { onMounted, reactive, ref } from 'vue';
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 	
     const params = {
-        "latitude": 52.52,
-        "longitude": 13.41,
+        "latitude": '52.73',
+        "longitude": '15.22',
+        "current": "temperature_2m",
         "hourly": "temperature_2m"
     };
     const url = "https://api.open-meteo.com/v1/forecast";
-    const responses = await fetchWeatherApi(url, params);
-    
-    // Helper function to form time ranges
-    const range = (start: number, stop: number, step: number) =>
-        Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
-    
-    // Process first location. Add a for-loop for multiple locations or weather models
-    const response = responses[0];
-    
-    // Attributes for timezone and location
-    const utcOffsetSeconds = response.utcOffsetSeconds();
-    const timezone = response.timezone();
-    const timezoneAbbreviation = response.timezoneAbbreviation();
-    const latitude = response.latitude();
-    const longitude = response.longitude();
-    
-    const hourly = response.hourly()!;
-    
-    // Note: The order of weather variables in the URL query and the indices below need to match!
-    const weatherData = {
-    
-        hourly: {
-            time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
-                (t) => new Date((t + utcOffsetSeconds) * 1000)
-            ),
-            temperature2m: hourly.variables(0)!.valuesArray()!,
-        },
-    
-    };
-    console.log(weatherData)
+
+    const data = ref([]);
+    const chartData = reactive({
+        labels: [],
+        datasets: [
+            {
+                data: []
+            }
+        ]
+    })
+
+    const callAPI = async() => {
+        data.value = await ky.get(
+            url +'?'+ new URLSearchParams(params).toString()
+        ).json()
+
+        chartData.labels = data.value.hourly.time;
+        chartData.datasets[0].data = data.value.hourly.temperature_2m;
+
+        console.log(chartData)
+    }
+
+    onMounted(() => {
+        callAPI()
+    })
+
     
 </script>
 
 <template>
-    <div v-for="(elem, index) in weatherData.hourly.time" :key="index">
-       <div>{{elem}}: {{weatherData.hourly.temperature2m[index]}}</div>
-    </div>
+    <Bar v-if="chartData.labels[0]" id="chart" :data="chartData"/>
 </template>
 
 <style lang="postcss" scoped>
