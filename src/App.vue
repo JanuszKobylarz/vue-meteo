2958e580c12628c2714c90caf406c18083eace3f
Janusz Kobylarz <j.kobylarz@live.com>
2024-11-12 18:35:16 +0100

    Get data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import MeteoDashboard from './components/Meteo/MeteoDashboard.vue';
+
 </script>
 
 <template>
@@ -10,8 +11,14 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <Suspense>
+      <template #fallback>
+        Loading...
+      </template>
+      <MeteoDashboard/>
+    </Suspense>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
